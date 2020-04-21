609e7c66953a577c1012d392a725a2df8e3ef868
Alexey Brambalov <44169757+AlexeyBrambalov@users.noreply.github.com>
2020-04-21 14:28:26 +0100

    covid-app

---
+++ build/precache-manifest.138c04f73da2d10a94f3038e933a28f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcdcea80790097e4c86fa65dabe464c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66b2d04e3f0cd86649f3",
+    "url": "/static/css/main.276e6b32.chunk.css"
+  },
+  {
+    "revision": "672dd6151e439d042769",
+    "url": "/static/js/2.e7a1918c.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.e7a1918c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66b2d04e3f0cd86649f3",
+    "url": "/static/js/main.ede0c2cb.chunk.js"
+  },
+  {
+    "revision": "d9a2df771ef5bda69662",
+    "url": "/static/js/runtime-main.6b30d221.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
